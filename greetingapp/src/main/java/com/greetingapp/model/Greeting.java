8aa2f374b561114dbdb1fbbb31a3cbece6fb79ca
Mohitsoni887 <mohitsoni9886921@gmail.com>
2025-02-22 15:58:25 +0530

    [Mohit Soni] Add. Using Greeting Controller return JSON for different HTTP Methods.

---
+++ greetingapp/src/main/java/com/greetingapp/model/Greeting.java
@@ -0,0 +1,20 @@
+package com.greetingapp.model;
+
+public class Greeting {
+    //UC2
+    private String message;
+
+    public Greeting() {}  // No-args constructor for JSON deserialization
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
